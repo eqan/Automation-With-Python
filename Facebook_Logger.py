fd5791e745803f470e81f6b190e31b627be17b91
eqan <eqan.ahmad123@gmail.com>
2021-08-06 13:03:51 +0500

    Adding Facebook Logger

--- Facebook_Logger.py
+++ Facebook_Logger.py
@@ -3,30 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities    
 from getpass import getpass
   
-gmail_id = input('Enter Email: ')
+facebook_id = input('Enter Email: ')
 password = getpass('Enter Password: ')
 capabilities = DesiredCapabilities.CHROME.copy()
 capabilities['goog:loggingPrefs'] = {'performance': 'ALL'}
 driver = webdriver.Chrome(executable_path="C:\chromedriver.exe", desired_capabilities=capabilities)
 
 try:
-    driver.get(r'https://accounts.google.com/signin/v2/identifier?continue='+\
-    'https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1'+\
-    '&flowName=GlifWebSignIn&flowEntry = ServiceLogin')
+    driver.get(r'https://www.facebook.com/')
     driver.implicitly_wait(30)
 
-    loginBox = driver.find_element_by_xpath('//*[@id ="identifierId"]')
-    loginBox.send_keys(gmail_id)
+    loginBox = driver.find_element_by_xpath('//*[@name ="email"]')
+    loginBox.send_keys(facebook_id)
 
-    nextButton = driver.find_elements_by_xpath('//*[@id ="identifierNext"]')
-    nextButton[0].click()
-
-    passWordBox = driver.find_element_by_xpath('//*[@id ="password"]')
+    passWordBox = driver.find_element_by_xpath('//*[@id ="pass"]')
     passWordBox.send_keys(password)
 
-    nextButton = driver.find_elements_by_xpath('//*[@id ="passwordNext"]')
+    nextButton = driver.find_elements_by_xpath('//*[@data-testid="open-registration-form-button"]')
     nextButton[0].click()
     
+    driver.implicitly_wait(5)
     print('Login Successful!')
 except Exception as inst:
     print(inst)
