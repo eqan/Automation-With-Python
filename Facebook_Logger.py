34eff4af7875a6e7af4630c1bd1037d0ced64b36
eqan <eqan.ahmad123@gmail.com>
2021-08-06 14:22:11 +0500

    Adding FaceBook & Gmail

--- Facebook_Logger.py
+++ Facebook_Logger.py
@@ -2,6 +2,7 @@ import json
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities    
 from getpass import getpass
+import time
   
 facebook_id = input('Enter Email: ')
 password = getpass('Enter Password: ')
@@ -19,10 +20,14 @@ try:
     passWordBox = driver.find_element_by_xpath('//*[@id ="pass"]')
     passWordBox.send_keys(password)
 
-    nextButton = driver.find_elements_by_xpath('//*[@data-testid="open-registration-form-button"]')
+    nextButton = driver.find_elements_by_xpath('//*[@name="login"]')
     nextButton[0].click()
-    
-    driver.implicitly_wait(5)
+
+    time.sleep(100)
+    time_wait = 5*1000
+    #driver.implicitly_wait(time_wait)
+    #driver.set_page_load_timeout(time_wait)
+    #driver.set_script_timeout(time_wait)
     print('Login Successful!')
 except Exception as inst:
     print(inst)
